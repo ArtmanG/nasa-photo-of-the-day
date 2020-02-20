0ebfd9ecb561cd2f35b2c514761bb3f08c5f46ba
Gregory Hawman <g.hawman@gmail.com>
2020-02-20 13:31:01 -0800

    finished the explanation section and fixed some formatting across all three components so it is easier for me to read.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Image = ({hdurl}) => {
-    return <img scr={hdurl} alt='NASA Image' />
+    return (
+        <img scr={hdurl} alt='NASA Image' />
+    ); 
 };
 
 export default Image;
