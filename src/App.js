08d129430a4f2abe1aa38f4fb673e7e1edb59ed8
Gregory Hawman <g.hawman@gmail.com>
2020-02-20 13:19:53 -0800

    got the axios working and can see the data.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 
 import "./App.css";
+import Axios from 'axios';
 
 function App() {
+  const [nasaData, setNasaData] = useState({})
+  useEffect(()  => {
+    Axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => {
+        console.log(response.data);
+        setNasaData(response.data);
+      })
+      .catch(error => {console.log('Error Will Robinson! Error:', error)})
+  }, []);
+
   return (
     <div className="App">
       <p>
